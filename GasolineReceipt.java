bdab00d10a0bb9e9860b56f91402192036b28729
Josiah Moye <jmoye@neo.rr.com>
2016-04-29 10:02:02 -1000

    Exercise 2 study drills

--- GasolineReceipt.java
+++ GasolineReceipt.java
@@ -2,14 +2,14 @@ public class GasolineReceipt {
 	public static void main(String[] args) {
 		System.out.println( "+------------------------+" );
 		System.out.println( "|                        |" );
-		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|      COSTCO GAS        |" );
 		System.out.println( "|                        |" );
-		System.out.println( "| 2015-03-29 04:38PM     |" );
+		System.out.println( "| 2016-04-29 04:38PM     |" );
 		System.out.println( "|                        |" );
 		System.out.println( "| Gallons:       10.870  |" );
-		System.out.println( "| Price/gallon: $ 2.089  |" );
+		System.out.println( "| Price/gallon: $ 2.599  |" );
 		System.out.println( "|                        |" );
-		System.out.println( "| Fuel total:  $ 22.71   |" );
+		System.out.println( "| Fuel total:  $ 28.25   |" );
 		System.out.println( "|                        |" );
 		System.out.println( "+------------------------+" );
 	}
