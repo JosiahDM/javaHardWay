fbb07fb002162d0f5b29f26c1d7409fdd728852a
Josiah Moye <jmoye@neo.rr.com>
2016-05-01 10:51:29 -1000

    Exercise 5 + study drills added

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -1,20 +1,24 @@
 public class CreatingVariables {
 	public static void main( String[] args ) {
-		int x, y, age;
-		double seconds, e, checking;
-		String firstName, lastName, title;
+		int x, y, age, days;
+		double seconds, e, checking, pi;
+		String firstName, lastName, title, city, state;
 
 		x = 10;
 		y = 400;
 		age = 39;
+		days = 31;
 
 		seconds = 4.71;
 		e = 2.71828182845904523536;
 		checking = 1.89;
+		pi = 3.14159265359;
 
 		firstName = "Graham";
 		lastName = "Mitchell";
 		title = "Mr.";
+		city = "Lahaina";
+		state = "Hawaii";
 
 		System.out.println( "The variable x contains " + x );
 		System.out.println( "The value " + y + " is stored in the variable y." );
@@ -22,5 +26,9 @@ public class CreatingVariables {
 		System.out.println( "A favorite irrational # is Euler's number: " + e );
 		System.out.println( "Hopefully you have more than $" + checking + "!" );
 		System.out.println( "My name's " + title + " " + firstName + lastName );
+
+		// My additions for study drills
+		System.out.println( "There are " + days + " in the month of May." );
+		System.out.println( "I live in " + city + ", " + state + "." );
 	}
 }
