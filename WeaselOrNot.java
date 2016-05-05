e8a35e1d4106efa84e332788f37dc628bd040046
Josiah Moye <jmoye@neo.rr.com>
2016-05-04 15:51:46 -1000

    Exercise 13 updated shortened version

--- WeaselOrNot.java
+++ WeaselOrNot.java
@@ -4,15 +4,10 @@ public class WeaselOrNot {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		String word;
-		boolean yep, nope;
 
 		System.out.println( "Type the word \"weasel\" please." );
 		word = keyboard.next();
 
-		yep = "weasel".equals(word);
-		nope = !"weasel".equals(word);
-
-		System.out.println( "You typed what was requested: " + yep);
-		System.out.println( "You ignored polite instructions: " + nope);
+		System.out.println( "Requested word typed: " +  word.equals("weasel") );
 	}
 }
